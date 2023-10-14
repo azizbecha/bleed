43c293576daede4e868cc7ff97a4687af6670c0c
Aziz Becha <aziz07becha@gmail.com>
2023-10-14 04:07:13 +0100

    added Title component

---
+++ components/Title.tsx
@@ -0,0 +1,17 @@
+import { TbSquareRotatedFilled } from "react-icons/tb"
+
+export const Title: React.FC<{ title: string }> = ({ title }) => {
+    const iconStyle = {
+        display: 'inline',
+        verticalAlign: 'middle', // This property ensures vertical alignment with the text
+        marginRight: '5px' // Add spacing between the icon and the text
+    };
+
+    return (
+        <div className="flex inline-flex text-center mx-auto text-white">
+            <h1 className="text-center font-extrabold text-4xl mt-5">
+                <TbSquareRotatedFilled size={15} style={iconStyle} /> {title} <TbSquareRotatedFilled size={15} style={iconStyle} />
+            </h1>
+        </div>
+    )
+}
