088ab07fdd5b77dab1f72af43ef52fab11f64067
Aziz Becha <aziz07becha@gmail.com>
2023-10-14 02:04:21 +0100

    added initial Navbar component

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        primary: "#164dff"
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
