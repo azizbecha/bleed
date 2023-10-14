784ffd954f54fdf6ae5bb3e7964ae80ad532fef5
Aziz Becha <aziz07becha@gmail.com>
2023-10-14 04:28:46 +0100

    added learn more button

--- app/page.tsx
+++ app/page.tsx
@@ -4,17 +4,25 @@ import { Hero } from '@/components/Hero'
 import { Title } from '@/components/Title'
 import { Divider } from '@/components/Divider'
 
+import { IoMdArrowDropright } from "react-icons/io"
+
 export default function Home() {
   return (
     <AOSWrapper>
       <Navbar />
       <Hero />
       <div className='mx-auto max-w-screen-xl mt-5 px-5 py-6 sm:px-6 lg:px-8'>
-        <div className="flex justify-center">
-          <div className="text-center text-white" id='#about'>
+        <div className="flex justify-center text-center">
+          <div className="text-white" id='#about'>
             <Title title='Who are We' />
             <h1 className='mt-3'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi inventore eligendi quam quasi iure? At laudantium natus praesentium et id perspiciatis ad alias aliquam ipsam in tempore cumque, itaque atque.</h1>
           
+            <button className='flex items-center font-mono px-4 py-2 mt-5 hover:rounded-xl border-2 border-white mx-auto text-center'>
+              Learn More
+              <IoMdArrowDropright size={19} className='ml-2' />
+            </button>
+
+
             <Divider />
             <Title title='Products' />
 
