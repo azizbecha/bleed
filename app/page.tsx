1a008845ca828da15a9df64e6ca82b90862f4095
Aziz Becha <aziz07becha@gmail.com>
2023-10-24 11:17:47 +0100

    added NoteSlider as a reusable component

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,7 @@ import { Divider } from '@/components/Divider'
 import { ProductsRow } from '@/components/ProductsRow'
 import { Container } from '@/components/Container'
 import { Footer } from '@/components/Footer'
+import { NoteSlider } from '@/components/NoteSlider'
 
 import { IoMdArrowDropright } from "react-icons/io"
 
@@ -19,12 +20,7 @@ import VisibilitySensor from 'react-visibility-sensor'
 export default function Home() {
   return (
     <AOSWrapper>
-      <div className="bg-blue-500 text-white text-center py-2">
-        <div className="scrolling-text flex">
-          <img className='h-6' src='../BLEED DELIVERY_.png' /> We provide ship orders over all Tunisia. Please don't hesitate to reach out to us if you have any questions. Our customer support is available 24/7. <img className='h-6' src='../222.png' />
-        </div>
-      </div>
-
+      <NoteSlider />
       <Navbar />
       <Hero />
 
