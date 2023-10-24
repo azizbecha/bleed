848846b5a4ee5cf075e8ca5beb5c28c787ed378b
Aziz Becha <aziz07becha@gmail.com>
2023-10-25 00:14:49 +0100

    fixed NoteSlider sentence

--- components/NoteSlider.tsx
+++ components/NoteSlider.tsx
@@ -2,7 +2,7 @@ export const NoteSlider: React.FC = () => {
     return (
         <div className="bg-blue-500 text-white text-center py-2">
             <div className="scrolling-text flex">
-            <img className='h-6' src='../BLEED DELIVERY_.png' /> We provide ship orders over all Tunisia. Please don't hesitate to reach out to us if you have any questions. Our customer support is available 24/7. <img className='h-6' src='../222.png' />
+                <img className='h-6' src='../BLEED DELIVERY_.png' /> We ship orders throughout Tunisia. Please don't hesitate to contact us if you have any questions. Our customer support is available 24/7. <img className='h-6' src='../222.png' />
             </div>
         </div>
     )
