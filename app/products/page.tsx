fa1819137903dea6e17dc11288e82b8c76348c12
Aziz Becha <aziz07becha@gmail.com>
2023-10-24 13:36:08 +0100

    added main products page

---
+++ app/products/page.tsx
@@ -0,0 +1,23 @@
+import AOSWrapper from "@/components/AOSWrapper";
+import { Container } from "@/components/Container";
+import { Footer } from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+import { ProductsRow } from "@/components/ProductsRow";
+import { Title } from "@/components/Title";
+
+export default function Product() {
+    return (
+        <AOSWrapper>
+            <Navbar />
+            <Container>
+                <div className="flex justify-center text-center">
+                    <div>
+                        <Title title="Products" />
+                        <ProductsRow />
+                    </div>
+                </div>
+            </Container>
+            <Footer />
+        </AOSWrapper>
+    )
+}
